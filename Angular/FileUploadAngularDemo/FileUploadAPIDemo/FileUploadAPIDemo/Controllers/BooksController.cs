a127a78f859a168403f12fc444dfe8016722cea5
Sonu Sathyadas <sonusathyadas@hotmail.com>
2019-08-27 14:50:10 +0530

    angular file upload

---
+++ Angular/FileUploadAngularDemo/FileUploadAPIDemo/FileUploadAPIDemo/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUploadAPIDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<Book> Create([FromForm]Book book)
+        {
+
+            return new Book
+            {
+                BookName=book.BookName,
+                Author=book.Author
+            };
+        }
+    }
+}
---
+++ Angular/FileUploadAngularDemo/FileUploadAPIDemo/FileUploadAPIDemo/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadAPIDemo.Models
+{
+    public class Book
+    {
+        [Required]
+        [JsonProperty(PropertyName ="bookName")]
+        public string BookName { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName ="author")]
+        public string Author { get; set; }
+
+        [JsonProperty(PropertyName ="bookFile")]
+        public IFormFile BookFile { get; set; }
+    }
+}
